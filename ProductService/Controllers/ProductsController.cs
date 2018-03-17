2c718c88f94d44089dcd4b15c17319334f3e5f9f
Teresa Ritorto <tritorto@seek.com.au>
2018-03-17 17:23:50 +1100

    stubbing out project

---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using ProductService.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ProductService.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        //TODO - In-memory repo for products - use DI to pass into controller
+        
+        //GET products
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+          
+        }
+
+        //GET products/{id}
+        public IHttpActionResult GetProduct(string id)
+        {
+            throw new NotImplementedException();          
+        }
+    }
+}
